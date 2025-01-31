03f8afcaa6012022b0b10120881e858061b367be
devang <d3vang2002@gmail.com>
2025-01-31 21:07:24 +0530

    added config files

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -7,6 +7,8 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        borderWidth:2,
+        borderColor: "red"
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
