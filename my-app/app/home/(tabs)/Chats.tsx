01b3e9083fcc4d3866661058fa0e9e573bbc8716
devang <d3vang2002@gmail.com>
2025-02-17 12:57:14 +0530

    Recommit

--- my-app/app/home/(tabs)/Chats.tsx
+++ my-app/app/home/(tabs)/Chats.tsx
@@ -7,7 +7,7 @@ export default function Chats() {
     
 
     return(
-        <View className="flex-1 flex` border-2 border-red-600">
+        <View className="flex-1 flex` border-2 bg-black">
             <Text>Profile Tab</Text>
         </View>
     )
