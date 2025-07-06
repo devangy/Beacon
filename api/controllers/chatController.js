072b0cb776784908926b5e4c34aa1c32e436f1d6
devangnpm <d3vang2002@gmail.com>
2025-07-06 18:02:13 +0530

    start new chat api creation and add new user btn  addition

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -1,9 +1,87 @@
+import { prisma } from "../utils/prismaClient.js";
+import dotenv from "dotenv";
+dotenv.config();
 
+export const getUserChats = async (req, res) => {
+  //   const token = req.headers.authorization.split(" ")[1]; // extracting token by splitting Bearer Token into array and accesing token [1]
 
+  //   if (!token) res.status(401).json("No token provided for authentication");
 
+  const chats = await prisma.chat.findMany({
+    where: {
+      members: {
+        some: {
+          userId: userId,
+        },
+      },
+    },
+    include: {
+      members: {
+        include: { user: true },
+      },
+    },
+  });
 
+  console.log("allchat", chats);
+};
 
+const userId = "ae2ac7dc-403f-48ba-8dfe-e1837a2db958";
 
-export const getUserChats = async () => {
-    
-}
+// getUserChats(userId)
+
+export const startNewChat = async (req, res) => {
+  const { friendId } = req.body;
+  console.log("friend here:", friendId);
+
+  // check if a chat with this friendId exists in DB
+
+  const existingChat = await prisma.chat.findFirst({
+    where: {
+      members: {
+        every: {
+          userId: {
+            in: [currentUserId, friendId],
+          },
+        },
+      },
+    },
+    include: {
+      members: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
+
+  if (!existingChat) {
+    res.status(200).json({
+      success: true,
+      message: "Existing chat with this user already exists",
+      data: existingChat,
+    });
+  }
+
+  // create a chat with both the user and the user's friendId
+
+    const newChat = await prisma.chat.create({
+      data: {
+        members: {
+          create: [
+            { user: { connect: { id: currentUserId } } },
+            { user: { connect: { id: friendId } } },
+          ],
+        },
+      },
+    });
+
+  res.status(200).json({
+    success: true,
+    message: "New Chat created with the user",
+    data: {
+      data: newChat,
+    },
+  });
+};
+
+// startNewChat(userId, friendId);
